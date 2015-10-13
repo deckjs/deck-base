8e0eebd0584c1dc1d52a6db8fdd7d61fb0f6534b
Cristian Kiss <cristian.kiss@nearform.com>
2015-10-13 11:21:03 +0300

    added standard + standard format fixes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var fs = require('fs');
-var path = require('path');
+var fs = require('fs')
+var path = require('path')
 
-module.exports = require('./package.json');
-module.exports.deck = fs.readFileSync(path.join(__dirname, 'deck.md'));
+module.exports = require('./package.json')
+module.exports.deck = fs.readFileSync(path.join(__dirname, 'deck.md'))
